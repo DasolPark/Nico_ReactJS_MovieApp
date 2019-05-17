2c6ab06e87c4d3da71c0d63a0a522cc1b57a37db
Dasol Park <dasol.park615@gmail.com>
2019-05-17 16:03:36 +0800

    Components and Props 2.2 Dataflow with Props

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import Movie from "./Movie";
 
+const movieTitles = ["Matrix", "Full Metal Jacket", "Oldboy", "Star Wars"];
+const movieImages = [
+  "https://neo1210.files.wordpress.com/2010/09/matrix_movie1.jpg",
+  "https://www.avcesar.com/source/software/tmdb/1431/couv_full-metal-jacket.jpg",
+  "http://fims.kofic.or.kr/upload/up_img/cleansing/50/mov_20030350_16.jpg",
+  "https://coffeeandgeeks.files.wordpress.com/2015/01/star-wars-the-force-awakens-fan-poster-inspired-by-drew-struzan.jpeg"
+];
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload. Hello World!!
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Movie title={movieTitles[0]} poster={movieImages[0]} />
+        <Movie title={movieTitles[1]} poster={movieImages[1]} />
+        <Movie title={movieTitles[2]} poster={movieImages[2]} />
+        <Movie title={movieTitles[3]} poster={movieImages[3]} />
       </div>
     );
   }
