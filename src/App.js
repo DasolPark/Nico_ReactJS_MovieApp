976ccc73cace96269b79d1cb30a389c5ba98d276
Dasol Park <dasol.park615@gmail.com>
2019-05-21 15:47:51 +0800

    4.2 Practicing this.setState()

--- src/App.js
+++ src/App.js
@@ -2,49 +2,55 @@ import React, { Component } from "react";
 import "./App.css";
 import Movie from "./Movie";
 
-const movies = [
-  {
-    title: "Matrix",
-    poster: "https://neo1210.files.wordpress.com/2010/09/matrix_movie1.jpg"
-  },
-  {
-    title: "Full Metal Jacket",
-    poster:
-      "https://www.avcesar.com/source/software/tmdb/1431/couv_full-metal-jacket.jpg"
-  },
-  {
-    title: "Old boy",
-    poster:
-      "http://fims.kofic.or.kr/upload/up_img/cleansing/50/mov_20030350_16.jpg"
-  },
-  {
-    title: "Star Wars",
-    poster:
-      "https://coffeeandgeeks.files.wordpress.com/2015/01/star-wars-the-force-awakens-fan-poster-inspired-by-drew-struzan.jpeg"
-  }
-];
-
 class App extends Component {
   // Render: componentWillMount() -> render() -> componentDidMound()
   // Update: componentWillReceiveProps() -> shouldComponentUpdate() == true -> componentWillUpdate() -> render() -> componentDidUpdate()
 
   state = {
-    greeting: "Hello!"
+    greeting: "Hello!",
+    movies: [
+      {
+        title: "Matrix",
+        poster: "https://neo1210.files.wordpress.com/2010/09/matrix_movie1.jpg"
+      },
+      {
+        title: "Full Metal Jacket",
+        poster:
+          "https://www.avcesar.com/source/software/tmdb/1431/couv_full-metal-jacket.jpg"
+      },
+      {
+        title: "Old boy",
+        poster:
+          "http://fims.kofic.or.kr/upload/up_img/cleansing/50/mov_20030350_16.jpg"
+      },
+      {
+        title: "Star Wars",
+        poster:
+          "https://coffeeandgeeks.files.wordpress.com/2015/01/star-wars-the-force-awakens-fan-poster-inspired-by-drew-struzan.jpeg"
+      }
+    ]
   };
 
   componentDidMount() {
     setTimeout(() => {
       this.setState({
-        greeting: "Hello again!"
+        movies: [
+          {
+            title: "Trainspotting",
+            poster:
+              "https://coffeeandgeeks.files.wordpress.com/2015/01/star-wars-the-force-awakens-fan-poster-inspired-by-drew-struzan.jpeg"
+          },
+          ...this.state.movies
+        ]
       });
-    }, 2000);
+    }, 3000);
   }
 
   render() {
     return (
       <div className="App">
         {this.state.greeting}
-        {movies.map((movie, index) => {
+        {this.state.movies.map((movie, index) => {
           return (
             <Movie title={movie.title} poster={movie.poster} key={index} />
           );
